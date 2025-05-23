d2831cd61d7e16acb29f745402f0990cba0d7636
rojan.panta <rojan.panta@swifttech.com.np>
2025-05-23 15:54:25 +0545

    init

---
+++ service-one/src/test/java/com/example/multimodule/application/DemoApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.multimodule.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+public class DemoApplicationTest {
+
+
+//    @Test
+//    public void contextLoads() {
+//
+//    }
+
+}
---
+++ service-two/src/test/java/com/example/multimodule/application/DemoApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.multimodule.application;
+
+//@SpringBootTest
+public class DemoApplicationTest {
+
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+
+}
